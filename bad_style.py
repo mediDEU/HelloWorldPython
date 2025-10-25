e1c2ce3a591d52c998850d0571d93df7651f6ca4
mediDEU <mehdivasef@gmail.com>
2025-10-25 19:18:03 +0200

    Identify style violations in bad_style.py
    test

---
+++ bad_style.py
@@ -0,0 +1,10 @@
+# Many style violations: wildcard import, unused import, wrong naming, missing spaces, long line
+from math import *
+import os,sys
+
+def badFunction():  # function name not snake_case
+    x=1+2  # missing spaces around operators
+    long_line = "This is a very long string intended to exceed the typical 79 character line length limit used by many linters and style guides."  # noqa: E501
+    unused_var = 42
+    print("Hello, world!")  
+    return x
