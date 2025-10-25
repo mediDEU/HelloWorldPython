86899e9541ff83aeac4f1124e013f6768d7e5472
mediDEU <mehdivasef@gmail.com>
2025-10-25 20:04:34 +0200

    Update bad_style.py
    t

--- bad_style.py
+++ bad_style.py
@@ -1,9 +1,8 @@
-# Many style violations: wildcard import, unused import, wrong naming, missing spaces, long line
 from math import *
 import os,sys
 
-def badFunction():  # function name not snake_case
-    x=1+2  # missing spaces around operators
+def badFunction():
+    x=1+2 
     long_line = "This is a very long string intended to exceed the typical 79 character line length limit used by many linters and style guides."  # noqa: E501
     unused_var = 42
     print("Hello, wwwww!")  
