b9a34c9f5f1937d64fc4715e0ad0dab669610289
mediDEU <mehdivasef@gmail.com>
2025-10-25 19:45:00 +0200

    Update bad_style.py
    t

--- bad_style.py
+++ bad_style.py
@@ -6,5 +6,5 @@ def badFunction():  # function name not snake_case
     x=1+2  # missing spaces around operators
     long_line = "This is a very long string intended to exceed the typical 79 character line length limit used by many linters and style guides."  # noqa: E501
     unused_var = 42
-    print("Hello, welt!")  
+    print("Hello, wwww!")  
     return x
